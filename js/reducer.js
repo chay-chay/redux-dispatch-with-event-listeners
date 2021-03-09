a23b61685e3e229f52e9624ff28df10d9bf037df
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-08 23:11:59 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,20 +1,59 @@
 // add code snippets from README
-let state = {count: 0}
 
-function reducer(state, action){
+let state;
+function reducer(state = {count: 0}, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
-      return {count: state.count + 1};
+      return {count: state.count + 1}
     default:
       return state;
   }
-} 
+}
 
 function dispatch(action){
-    state = reducer(state, action);
-  } // 
+  state = reducer(state, action);
+  render(); // to update state
+}
 
-  function render(){
-    let container = document.getElementById('container');
-    container.textContent = state.count;
-  } 
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+} // Use the render function to display our state.
+
+dispatch({type: '@@INIT'})
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+}) 
+
+
+
+// let state = {count: 0}
+
+// function reducer(state, action){
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1};
+//     default:
+//       return state;
+//   }
+// } 
+
+// function dispatch(action){
+//     state = reducer(state, action);
+//   } // to update state
+
+//   function render(){
+//     let container = document.getElementById('container');
+//     container.textContent = state.count;
+//   } // Use the render function to display our state.
+
+//   dispatch({type: '@@INIT'}) 
+
+//   let button = document.getElementById('button');
+
+// button.addEventListener('click', () => {
+//   dispatch({type: 'INCREASE_COUNT'})
+// }) 
